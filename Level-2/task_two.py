ccc49b06439e45c155373ef3ac164534c7b5f0a6
Jo Kellock <trainee.jo.kellock@sigmalabs.co.uk>
2026-06-19 16:13:35 +0100

    Still one error

--- Level-2/task_two.py
+++ Level-2/task_two.py
@@ -12,13 +12,49 @@ basket = []
 #####
 
 
-def add_to_basket(item: dict) -> list:
-    pass
+def add_to_basket(item: dict) -> list[dict]:
+    """Adds an item and its price to a list."""
+    basket.append(item)
+    return basket
 
+
+def duplicate_count(item_price: str, item_name: str, basket: list) -> dict:
+    count = 0
+    for item in basket:
+        if item["price"] == item_price:
+            if item["name"] == item_name:
+                count += 1
+    return count
 
 
 def generate_receipt(basket: list) -> str:
-    pass
+    """Generates a receipt for a basket of items."""
+    receipt = ""
+    item_list = []
+    total_price = 0
+
+    if len(basket) >= 1:
+        for item in basket:
+            no_of_duplicates = duplicate_count(
+                item["price"], item["name"], basket)
+            item_list.append(item["name"])
+            if item["price"] != 0:
+                receipt += f"{item["name"]} x {no_of_duplicates} - £{item["price"] * no_of_duplicates:.2f}" + "\n"
+
+            else:
+                receipt += f"{item["name"]} - Free" + "\n"
+
+            if no_of_duplicates > 1:
+                basket.remove(item)
+
+            total_price += item["price"] * no_of_duplicates
+
+        receipt += f"Total: £{total_price:.2f}"
+
+    else:
+        receipt = "Basket is empty"
+
+    return receipt  # return the receipt string
 
 #####
 #
