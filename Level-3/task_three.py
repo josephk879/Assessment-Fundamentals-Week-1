f6738b81ea1cae989b2978a433c3544045a21cde
Jo Kellock <trainee.jo.kellock@sigmalabs.co.uk>
2026-06-19 17:00:32 +0100

    Making 2 look nice and first effort at 3

--- Level-3/task_three.py
+++ Level-3/task_three.py
@@ -4,8 +4,26 @@ Note: Do not add ANY variables to the global scope. This WILL break the tests.
 
 
 def generate_invoice(receipt_string: str) -> str:
+    """Generates a VAT invoice."""
+    receipt_line_list = receipt_string.splitlines()
+    vat_receipt = "VAT RECEIPT" + "\n" + "\n"
 
-    return  # return the invoice string
+    for line in receipt_line_list[:-1]:
+        item_before_vat = format((float(line[-4:]) * 0.8), ".2f")
+
+        vat_receipt += line.replace(line[-5:], "£" +
+                                    str(item_before_vat)) + "\n"
+
+    total_before_vat = format((float(receipt_line_list[-1][-4:]) * 0.8), ".2f")
+
+    vat = format(
+        float(receipt_line_list[-1][-4:]) - float(total_before_vat), ".2f")
+
+    vat_receipt += "\n" + "Total: £" + \
+        str(total_before_vat) + "\n" + "VAT: " + "£" + str(vat) + \
+        "\n" + "Total inc VAT: " + receipt_line_list[-1][-5:]
+
+    return vat_receipt  # return the invoice string
 
 
 if __name__ == "__main__":
