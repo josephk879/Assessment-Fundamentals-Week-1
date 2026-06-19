a46b3803dfa490e08dd51fab02f169b2855926e6
Jo Kellock <trainee.jo.kellock@sigmalabs.co.uk>
2026-06-19 15:09:36 +0100

    Completed task 1

--- Level-1/task_one.py
+++ Level-1/task_one.py
@@ -7,13 +7,26 @@ The only variable you are allowed to use in the global scope is the basket below
 basket = []
 
 
-def add_to_basket(item: dict) -> list:
+def add_to_basket(item: dict) -> list[dict]:
+    """Adds an item and its price to a list."""
     basket.append(item)
     return basket
 
 
 def generate_receipt(basket: list) -> str:
-    return  # return the receipt string
+    """Generates a receipt for a basket of items."""
+    receipt = ""
+    if len(basket) >= 1:
+        for item in basket:
+            if item["price"] != 0:
+                receipt += f"{item["name"]} - £{item["price"]:.2f}" + "\n"
+            else:
+                receipt += f"{item["name"]} - Free" + "\n"
+        total_price = sum(item["price"] for item in basket)
+        receipt += f"Total: £{total_price:.2f}"
+    else:
+        receipt = "Basket is empty"
+    return receipt  # return the receipt string
 
 
 if __name__ == "__main__":
